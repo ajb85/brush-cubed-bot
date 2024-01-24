04eeed03511eacd5ec720a5aa559467f34d0cbd2
AJ Brush <19802883+ajb85@users.noreply.github.com>
2024-01-23 16:26:24 -0800

    abstracts code entry to cloud function

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-require("./server");
-require("./discordServer");
+exports.init = (req, res) => {
+  require("./server");
+  require("./discordServer");
+  let message = req.query.message || req.body.message || "Hello World!";
+  res.status(200).send(message);
+};
